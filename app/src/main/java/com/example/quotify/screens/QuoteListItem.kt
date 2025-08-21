91c0eaf859a44deb8264d3dc1933a006a0e6521f
anshh-raj <anshambastha@gmail.com>
2025-08-21 13:23:09 +0530

    build the quote list screen

---
+++ app/src/main/java/com/example/quotify/screens/QuoteListItem.kt
@@ -0,0 +1,144 @@
+package com.example.quotify.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FormatQuote
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.quotify.R
+
+@Composable
+fun QuoteListItem(){
+    Card(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                imageVector = Icons.Filled.FormatQuote,
+                contentDescription = "Quote",
+                colorFilter = ColorFilter.tint(Color.White),
+                alignment = Alignment.TopStart,
+                modifier = Modifier
+                    .size(40.dp)
+                    .rotate(180F)
+                    .background(Color.Black)
+            )
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = "Time is the most valuable thing a man can spend",
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 8.dp)
+                )
+
+                Box(
+                    modifier = Modifier
+                        .background(Color(0xFFEEEEEE))
+                        .fillMaxWidth(.4f)
+                        .height(1.dp)
+                )
+
+                Text(
+                    text = "Theophrastus",
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Thin,
+                    modifier = Modifier.padding(top = 4.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun QuoteDetail(){
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize(1f)
+            .background(
+                brush =  Brush.sweepGradient(
+                    colors = listOf(
+                        Color(0xFFffffff),
+                        Color(0xFFE3E3E3)
+                    )
+                )
+            )
+    ) {
+
+        Card(
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 4.dp
+            ),
+            modifier = Modifier
+                .padding(32.dp)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .padding(16.dp, 24.dp)
+            ) {
+                Image(
+                    imageVector = Icons.Filled.FormatQuote,
+                    contentDescription = "Quote",
+                    modifier = Modifier
+                        .size(80.dp)
+                        .rotate(180F)
+                        .align(Alignment.CenterHorizontally)
+                )
+
+                Text(
+                    text = "Time is the most valuable thing a man can spend",
+                    fontFamily = FontFamily(Font(R.font.montserrat_regular)),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = "Theophrastus",
+                    fontFamily = FontFamily(Font(R.font.montserrat_regular)),
+                    style = MaterialTheme.typography.labelLarge
+                )
+            }
+        }
+
+    }
+}
